15516a19518dbbd1d90b95d63120f55804ae75d5
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-02-02 17:42:43 +0100

    Angular Material – 3 Formularios Reactivos (parte2)

--- src/app/formularios/form2/form2.component.ts
+++ src/app/formularios/form2/form2.component.ts
@@ -10,16 +10,28 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+const MATERIAL_MODULES = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatRadioModule,
+  MatCheckboxModule,
+  MatSelectModule,
+];
 
 @Component({
   selector: 'app-form2',
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-  ],
+  imports: [ReactiveFormsModule, MATERIAL_MODULES],
   templateUrl: './form2.component.html',
   styleUrl: './form2.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -31,7 +43,20 @@ export default class Form2Component {
     nombres: [''],
     apellidos: [''],
     email: ['', [Validators.required, Validators.email]],
+    genero: [''],
+    food: [''],
+    habilidades: this.fb.group({
+      liderazgo: [''],
+      comunicacion: [''],
+      empatia: [''],
+    }),
   });
+
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
 
   onSubmit() {
     console.log(this.personaForm.value);
